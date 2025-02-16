bfff42b482fab4e70507e483e5ecf19758da0cb2
Julien Talabaza <88831335+gunout@users.noreply.github.com>
2025-02-17 01:01:17 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,40 @@
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
-const http = require('http');
 const socketIo = require('socket.io');
 
 const app = express();
-const server = http.createServer(app);
+
+// Configurations HTTPS
+const options = {
+  key: fs.readFileSync('/home/gleaphe/Desktop/Cyber/localhost.key'), // Chemin vers votre fichier clé
+  cert: fs.readFileSync('/home/gleaphe/Desktop/Cyber/localhost.crt') // Chemin vers votre fichier certificat
+};
+
+// Création du serveur HTTPS
+const server = https.createServer(options, app);
 const io = socketIo(server);
 
-// Remplacez par vos données de flux réelles
-const streamData = "Données de flux";
+// Middleware pour servir des fichiers statiques
+app.use(express.static('public'));
 
-// Servir les fichiers statiques du dossier public
-app.use(express.static(__dirname + '/public'));
-
-app.get('/stream', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write(streamData);
-    res.end();
-});
-
+// Gestion des connexions Socket.IO
 io.on('connection', (socket) => {
-    console.log('Nouvel utilisateur connecté');
+    console.log('Un utilisateur est connecté');
 
     socket.on('broadcaster', (stream) => {
         socket.broadcast.emit('viewer', stream);
     });
 
     socket.on('disconnect', () => {
-        console.log('Utilisateur déconnecté');
+        console.log('Un utilisateur est déconnecté');
     });
 });
 
+// Port d'écoute
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
+    console.log(`Serveur en cours d'exécution sur https://localhost:${PORT}`);
 });
 
 
